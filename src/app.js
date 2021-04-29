3d83ab5284b223c565b69a95fe911f3fc218e538
Jasmina Zejnilovic <jasmina.zejnilovic995@gmail.com>
2021-04-29 14:55:45 +0200

    Mongo Server Project: cors package added

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
 import helmet from "helmet";
+import cors from "cors";
 // if only folder is specified, index.js is meant to be imported
 import router from "./router";
 
 const app = express();
 app.use(express.json());
 app.use(helmet());
+app.use(cors());
 
 router(app);
 
